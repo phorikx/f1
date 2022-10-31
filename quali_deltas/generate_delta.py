7cfe0571ed3da38360797148cbf307c6a9613dd7
Paul Horikx <paulhorikx@pop-os.localdomain>
2022-10-31 21:23:49 +0100

    added first quali time notebook

---
+++ quali_deltas/generate_delta.py
@@ -0,0 +1,23 @@
+import fastf1 as ff1
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ff1.Cache.enable_cache('cache')
+
+quali = ff1.get_session(2021, 'Turkey', 'Q')
+laps = quali.load_laps(with_telemetry=True)
+
+drivers = laps.Driver.unique()
+laptimes = []
+
+for driver in drivers:
+    laptimes.append(laps.pick_driver(driver).pick_fastest().LapTime.total_seconds())
+
+min_laptime = min(laptimes)
+for i, laptime in enumerate(laptimes):
+    laptimes[i] = (laptime/min_laptime -1) * 100
+
+df = pd.DataFrame({'drivers':drivers, 'laptimes': laptimes})
+
+df.plot(x='drivers', y='laptimes', kind='scatter')
+plt.show
